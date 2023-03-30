84431790db98dc97dd963fa56ef2c9e0667e674e
JuanDelgadillo11 <juan.pablo.delgadillo.peredo@gmail.com>
2023-03-30 12:28:54 -0400

    Adding request tcs

--- login.cy.js
+++ login.cy.js
@@ -2,4 +2,10 @@ describe('Login con credenciales que no tienen privilegios', () => {
     it('Authentication error is raised when user doesnot have privileges', () => {
         //codigo que permite realizar la operacion
     });
+	it('Crear un nuevo recurso', () => {
+        //codigo que permite crear un nuevo recurso
+    });
+	it('Crear un nuevo recurso3', () => {
+        //codigo que permite crear un nuevo recurso
+    });
 });
