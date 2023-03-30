b75b35a9938eba08b2b422409939cacde8fe634c
Juan Delgadillo <Juan.Delgadillo@windriver.com>
2023-03-30 11:31:18 -0400

    Added code for the tc

--- login.cy.js
+++ login.cy.js
@@ -1,5 +1,5 @@
 describe('Login con credenciales que no tienen privilegios', () => {
     it('Authentication error is raised when user doesnot have privileges', () => {
-        
+        //codigo que permite realizar la operacion
     });
 });
