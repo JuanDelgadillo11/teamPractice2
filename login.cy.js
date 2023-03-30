e7cb3ec6fe97bf57ad5e73f139a85e36c48b4bb3
jp4all <129407102+jp4all@users.noreply.github.com>
2023-03-30 13:10:34 -0400

    Added delete request (#3)
    Co-authored-by: Juan Delgadillo <Juan.Delgadillo@windriver.com>

--- login.cy.js
+++ login.cy.js
@@ -2,6 +2,8 @@ describe('Login con credenciales que no tienen privilegios', () => {
     it('Authentication error is raised when user doesnot have privileges', () => {
         //codigo que permite realizar la operacion
     });
+    it('Delete a request', () => {
+        //codigo que permite realizar la operacion
 	it('Crear un nuevo recurso', () => {
         //codigo que permite crear un nuevo recurso
     });
